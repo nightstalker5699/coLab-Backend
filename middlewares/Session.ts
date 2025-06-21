6bea5f7581adc0c5f45c9a93235b5cc99144527f
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2025-06-22 01:03:57 +0300

    session now use redis to store it

--- middlewares/Session.ts
+++ middlewares/Session.ts
@@ -1,6 +1,31 @@
 import Session from "express-session";
+import { createClient } from "redis";
+import { RedisStore } from "connect-redis";
 
+const redisClient = createClient({
+  socket: {
+    host: "redis",
+    port: 6379,
+  },
+});
+
+// Handle Redis connection
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error:", err);
+});
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redisClient.connect().catch(console.error);
+
+let store = new RedisStore({
+  client: redisClient,
+  prefix: "sessions:",
+});
 const sessionSettings = Session({
+  store: store,
   secret: process.env.SESSION_SECRET || "test",
   resave: false,
   saveUninitialized: false,
